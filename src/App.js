5108bf86bdd9f052e836b7db99aa4ee5c78cfcc0
Jonathan D'souza <jonathan07082001@gmail.com>
2023-08-27 15:41:21 +0530

    Initial Setup with Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Header } from "./Components/Header";
+import { Footer } from "./Components/Footer";
 
 function App() {
+  const [theme, setTheme] = useState(null);
+
+  const url =
+    //For Dark theme switcher
+    useEffect(() => {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        setTheme("dark");
+      } else {
+        setTheme("light");
+      }
+    }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header handleThemeSwitch={handleThemeSwitch} />
+      <div className="flex items-center justify-center h-screen pb-24 dark:bg-[#1a1a1b]">
+        <h1 className="text-white">Hello</h1>
+      </div>
+      <Footer />
     </div>
   );
 }
